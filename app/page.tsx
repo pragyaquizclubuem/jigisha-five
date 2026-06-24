3a9263575573be08785663e5676b7c72d6f03cf3
Supriya Mondal <info@ntskolkata.org>
2026-06-24 10:10:33 +0530

    #41 hero section created - 2026-06-24

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
-// import HomePage from "@/components/pages/HomePage";
-
-// export default function Home() {
-//   return (
-//     <div className="flex flex-col flex-1 items-center justify-center font-sans">
-//      <h1>Welcome to Jigisha !</h1>
-//     </div>
-//   );
-// }
-
 import PageWrapper from "@/components/wrappers/PageWrapper";
+import HomeHero from "@/components/home/HomeHero";
 import AboutUs from "@/components/sections/homepage/AboutUs";
 import Venue from "@/components/home/Venue";
 
 export default function Home() {
   return (
     <PageWrapper>
+      <HomeHero />
       <AboutUs />
       <Venue />
     </PageWrapper>
