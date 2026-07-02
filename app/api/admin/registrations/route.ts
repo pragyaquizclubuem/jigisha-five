321c0500bc90644167ae571e4563cd40de2a4e12
Barshan Banerjee <brsnprsnl@gmail.com>
2026-07-03 00:30:56 +0530

    #64 Jana Ojana Registration Page Created - 2026-07-03

--- app/api/admin/registrations/route.ts
+++ app/api/admin/registrations/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { prisma } from "@/lib/prisma";
-import { Prisma } from '@prisma/client';
+import { Prisma } from '.prisma/client';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
