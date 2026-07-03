7fce0806558765323dd7fc34e6d3a66b33a1fdc7
Barshan Banerjee <brsnprsnl@gmail.com>
2026-07-03 15:50:04 +0530

    #68 Contact Page Created - 2026-07-03

---
+++ components/pages/ContactPage.tsx
@@ -0,0 +1,8 @@
+import PageWrapper from "@/components/wrappers/PageWrapper";
+export default function ContactPage() {
+    return (
+        <PageWrapper>
+            <h1>Contact</h1>
+        </PageWrapper>
+    );
+}
