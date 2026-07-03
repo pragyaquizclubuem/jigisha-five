fb3007f637ea80d26d20e9683fd70eaad321ce31
Barshan Banerjee <brsnprsnl@gmail.com>
2026-07-03 07:22:25 +0530

    #55 Patch - 2026-07-03

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${robotoCondensed.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-[#ffede0]">
         <AudioProvider>
           <OverlayBanner />
           <Navbar />
